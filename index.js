3d25ce980a965f9e4faec109dc30e2dfb4c35739
RealByter <justavishay@gmail.com>
2023-03-05 00:09:54 +0200

    feat: fuck

---
+++ index.js
@@ -0,0 +1,195 @@
+let displayValue = '0';
+let firstOperand = null;
+let secondOperand = null;
+let firstOperator = null;
+let secondOperator = null;
+let result = null;
+const buttons = document.querySelectorAll('.button');
+
+function updateDisplay() {
+	const display = document.getElementById('display');
+	display.textContent = displayValue;
+	if (displayValue.length > 9) {
+		display.textContent = displayValue.substring(0, 9);
+	}
+}
+
+updateDisplay();
+
+function clickButton() {
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].addEventListener('click', function (e) {
+			const value = e.target.getAttribute('value');
+			if (e.target.classList.contains('operand')) {
+				inputOperand(value);
+				updateDisplay();
+			} else if (e.target.classList.contains('operator')) {
+				inputOperator(value);
+			} else if (e.target.classList.contains('equals')) {
+				inputEquals();
+				updateDisplay();
+			} else if (e.target.classList.contains('decimal')) {
+				inputDecimal(value);
+				updateDisplay();
+			} else if (e.target.classList.contains('percent')) {
+				inputPercent(displayValue);
+				updateDisplay();
+			} else if (e.target.classList.contains('sign')) {
+				inputSign(displayValue);
+				updateDisplay();
+			} else if (e.target.classList.contains('clear')) clearDisplay();
+			updateDisplay();
+		});
+	}
+}
+
+clickButton();
+
+function inputOperand(operand) {
+	if (firstOperator === null) {
+		if (displayValue === '0' || displayValue === 0) {
+			//1st click - handles first operand input
+			displayValue = operand;
+		} else if (displayValue === firstOperand) {
+			//starts new operation after inputEquals()
+			displayValue = operand;
+		} else {
+			displayValue += operand;
+		}
+	} else {
+		//3rd/5th click - inputs to secondOperand
+		if (displayValue === firstOperand) {
+			displayValue = operand;
+		} else {
+			displayValue += operand;
+		}
+	}
+}
+
+function inputOperator(operator) {
+	if (firstOperator != null && secondOperator === null) {
+		//4th click - handles input of second operator
+		secondOperator = operator;
+		secondOperand = displayValue;
+		result = operate(
+			Number(firstOperand),
+			Number(secondOperand),
+			firstOperator
+		);
+		displayValue = roundAccurately(result, 15).toString();
+		firstOperand = displayValue;
+		result = null;
+	} else if (firstOperator != null && secondOperator != null) {
+		//6th click - new secondOperator
+		secondOperand = displayValue;
+		result = operate(
+			Number(firstOperand),
+			Number(secondOperand),
+			secondOperator
+		);
+		secondOperator = operator;
+		displayValue = roundAccurately(result, 15).toString();
+		firstOperand = displayValue;
+		result = null;
+	} else {
+		//2nd click - handles first operator input
+		firstOperator = operator;
+		firstOperand = displayValue;
+	}
+}
+
+function inputEquals() {
+	//hitting equals doesn't display undefined before operate()
+	if (firstOperator === null) {
+		displayValue = displayValue;
+	} else if (secondOperator != null) {
+		//handles final result
+		secondOperand = displayValue;
+		result = operate(
+			Number(firstOperand),
+			Number(secondOperand),
+			secondOperator
+		);
+		if (result === 'lmao') {
+			displayValue = 'lmao';
+		} else {
+			displayValue = roundAccurately(result, 15).toString();
+			firstOperand = displayValue;
+			secondOperand = null;
+			firstOperator = null;
+			secondOperator = null;
+			result = null;
+		}
+	} else {
+		//handles first operation
+		secondOperand = displayValue;
+		result = operate(
+			Number(firstOperand),
+			Number(secondOperand),
+			firstOperator
+		);
+		if (result === 'lmao') {
+			displayValue = 'lmao';
+		} else {
+			displayValue = roundAccurately(result, 15).toString();
+			firstOperand = displayValue;
+			secondOperand = null;
+			firstOperator = null;
+			secondOperator = null;
+			result = null;
+		}
+	}
+}
+
+function inputDecimal(dot) {
+	if (displayValue === firstOperand || displayValue === secondOperand) {
+		displayValue = '0';
+		displayValue += dot;
+	} else if (!displayValue.includes(dot)) {
+		displayValue += dot;
+	}
+}
+
+function inputPercent(num) {
+	displayValue = (num / 100).toString();
+}
+
+function inputSign(num) {
+	displayValue = (num * -1).toString();
+}
+
+function clearDisplay() {
+	displayValue = '0';
+	firstOperand = null;
+	secondOperand = null;
+	firstOperator = null;
+	secondOperator = null;
+	result = null;
+}
+
+function inputBackspace() {
+	if (firstOperand != null) {
+		firstOperand = null;
+		updateDisplay();
+	}
+}
+
+function operate(x, y, op) {
+	if (op === '+') {
+		return x + y;
+	} else if (op === '-') {
+		return x - y;
+	} else if (op === '*') {
+		return x * y;
+	} else if (op === '/') {
+		if (y === 0) {
+			return 'lmao';
+		} else {
+			return x / y;
+		}
+	}
+}
+
+function roundAccurately(num, places) {
+	return parseFloat(Math.round(num + 'e' + places) + 'e-' + places);
+}
